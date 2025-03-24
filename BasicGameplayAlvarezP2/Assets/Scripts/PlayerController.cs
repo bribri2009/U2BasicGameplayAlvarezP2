c26b95e3d2181cfa0b95a1ff98bb66dd7daf2725
bribri2009 <735857@student.dusd.net>
2025-03-24 10:21:46 -0700

    worked on project
    aded scripts

--- BasicGameplayAlvarezP2/Assets/Scripts/PlayerController.cs
+++ BasicGameplayAlvarezP2/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
-    public float horizontalInput;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +11,22 @@ public class playerController : MonoBehaviour
     }
 
     // Update is called once per frame
+
+    public float horizontalInput;
+    public float speed = 10f;
+    public float xRange = 10f;
     void Update()
     {
+       if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+       if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
